7278fd8b5526024a890f32bae70a609881071b4e
Steelchar <steelchar90@gmail.com>
2016-09-25 00:43:04 -0700

    Update messages.js

--- client/reducers/messages.js
+++ client/reducers/messages.js
@@ -13,7 +13,7 @@ function messages(state = [], action) {
         {
           text: `
             <div class="text-center welcome">
-              <center><img src="http://media.pldh.net/pokemon/gen4/platinum/382.png" width="80" height="80"></center>
+              <center><img src="http://media.pldh.net/pokemon/gen5/blackwhite_animated_front/382.gif" width="107" height="83"></center>
               <h4><font color="#3399ff"><b>Welcome to the Hydrocity Video Application!</b></font></h3>
               <p><center>This video application is used for Anime Nights, Movie Nights, and sharing videos with other people.</center></p>
               <p><strong><font color="blue">Update: </strong>Playlists are added! - CreaturePhil</font></p>
